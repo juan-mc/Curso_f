aa8f66deaed87922435931021f65ad6e9723eb1c
juan-mc <jcamposl1301@alumno.ipn.mx>
2022-01-03 22:00:18 -0600

    Clase 16 y 17

---
+++ MOD_JS/Clase 16/js/ecma/main.js
@@ -0,0 +1,5 @@
+alert('hello world');
+const list=()=>{
+    return 10;
+};
+let nueva='perro';
---
+++ MOD_JS/Clase 17/js/main.js
@@ -0,0 +1,16 @@
+let wWindow=window.innerWidth;
+let hWindow=window.innerHeight;
+screen.width;
+screen.height;
+window.location;
+navigator.geolocalitation;
+const btn =document.getElementById('popup');
+//btn.addEventListener('click',getLocation);
+const frutas=['naranja','platano','manzana'];
+const frutas2='["naranja","platano","manzana"]';
+const frutasArray=JSON.parse(frutas2);
+//Para pasar al backend pasamos el objeto como un JSON
+const frutasJson=JSON.stringify(frutas);
+console.log(frutas);
+console.log(frutas2);
+console.log(frutasArray);
